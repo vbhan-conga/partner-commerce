979189efb7a6a6e7cd35ce5add463136aca33931
Christopher Moyle <chmoyle@apttus.com>
2020-01-23 15:45:52 -0500

    make_script file update

--- make_script.sh
+++ make_script.sh
@@ -24,7 +24,7 @@ function npm_install {
     rm -rf node_modules
     LOG_INFO "Update NPM"
     npm install -g npm
-    npm cache clean --force
+    npm config set registry https://registry.npmjs.com/
     LOG_INFO "NPM Install"
     npm install
 }
