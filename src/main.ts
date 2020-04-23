75506638f72eff373b4e97182e9571ff3c1b1c60
Christopher Moyle <chmoyle@apttus.com>
2020-04-23 16:12:39 -0400

    template updates

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+declare var __webpack_public_path__: string;
+const sv = (<any>window).sv;
+
 if (environment.production) {
   enableProdMode();
+  if (sv && sv.resource) {
+    __webpack_public_path__ = sv.resource + '/';
+  }
 }
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
