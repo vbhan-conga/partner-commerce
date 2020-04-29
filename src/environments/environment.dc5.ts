7f9ab5984f74c57008b2112c3de02c2b522461ed
Vijaya Anand <vanand@apttus.com>
2020-04-29 14:22:39 +0530

    Added separate env file for DC5

---
+++ src/environments/environment.dc5.ts
@@ -0,0 +1,32 @@
+import { Configuration } from '@apttus/core';
+export const environment: Configuration = {
+  production: true,
+  defaultImageSrc: './assets/images/default.png',
+  defaultCountry: 'US',
+  defaultLanguage: 'en-US',
+  enableErrorLogging: false,
+  enableErrorReporting: false,
+  enableMultiCurrency: false,
+  enableQueryLogs: true,
+  enablePerformanceLogs: true,
+  defaultCurrency: 'USD',
+  bufferTime: 10,
+  maxBufferSize: 5,
+  disableBuffer: false,
+  subqueryLimit: 10,
+  disableCache: false,
+  encryptResponse: false,
+  cartRetryLimit: 3,
+  productIdentifier: 'Id',
+  type: 'Salesforce',
+  debounceTime: 1000,
+  proxy: 'https://apttus-proxy.herokuapp.com',
+  useIndexedDB: true,
+  expandDepth: 8,
+  hashRouting: false,
+  // Salesforce environment variables
+  storefront: 'P-Commerce',
+  sentryDsn: 'https://6ad10246235742dc89f89b4c3f53f4aa@sentry.io/1230495',
+  organizationId: '00D3I0000008mFM',
+  endpoint: 'https://dc5-cpqqacommunity1.cs123.force.com/partners'
+};
