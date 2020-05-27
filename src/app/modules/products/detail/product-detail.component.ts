3e2511399f13403c3a797c3a77701b8a24354310
divinabopanna <dboppana@apttus.com>
2020-05-27 16:48:59 +0530

    DC-5113 Fixed issue with  "You have unsaved changes" pop up not displayed

--- src/app/modules/products/detail/product-detail.component.ts
+++ src/app/modules/products/detail/product-detail.component.ts
@@ -59,7 +59,7 @@ export class ProductDetailComponent implements OnInit {
    */
   onConfigurationChange(result: any) {
     this.cartItemList = _.first(result);
-    if (_.get(result[1], 'optionChanged') || _.get(result[1], 'attributeChanged')) {
+    if (_.get(result[1], 'optionChanged') || _.get(result[1], 'attributeChanged') || (_.isBoolean(result[1])) && result[1]) {
       this.configurationChanged = true;
     }
   }
