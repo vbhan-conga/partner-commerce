7987a135279602c66913105ef59803eb6d66e506
divinabopanna <dboppana@apttus.com>
2020-05-29 21:12:00 +0530

    DC-5129

--- src/app/modules/assets/list/asset-list.component.ts
+++ src/app/modules/assets/list/asset-list.component.ts
@@ -458,7 +458,7 @@ export class AssetListComponent implements OnInit, OnDestroy {
         label: 'Renew',
         theme: 'primary',
         validate(record: AssetLineItemExtended): boolean {
-          return record.canRenew() && !_.includes(_.map(_.get(cart, 'LineItems'), 'ProductId'), _.get(record, 'ProductId'));
+          return record.canRenew() && !(_.filter(_.get(cart, 'LineItems'), (item) => item.AssetLineItem.Id ===  record.Id).length > 0);
         },
         action: (recordList: Array<AObject>): Observable<void> => {
           this.assetModalService.openRenewModal(
@@ -474,7 +474,7 @@ export class AssetListComponent implements OnInit, OnDestroy {
         label: 'Terminate',
         theme: 'danger',
         validate(record: AssetLineItemExtended): boolean {
-          return record.canTerminate() && !_.includes(_.map(_.get(cart, 'LineItems'), 'ProductId'), _.get(record, 'ProductId'));
+          return record.canTerminate() && !(_.filter(_.get(cart, 'LineItems'), (item) => item.AssetLineItem.Id ===  record.Id).length > 0);
         },
         action: (recordList: Array<AObject>): Observable<void> => {
           this.assetModalService.openTerminateModal(
@@ -490,7 +490,7 @@ export class AssetListComponent implements OnInit, OnDestroy {
         label: 'Buy More',
         theme: 'primary',
         validate(record: AssetLineItemExtended): boolean {
-          return record.canBuyMore() && !_.includes(_.map(_.get(cart, 'LineItems'), 'ProductId'), _.get(record, 'ProductId'));
+          return record.canBuyMore() && !(_.filter(_.get(cart, 'LineItems'), (item) => item.AssetLineItem.Id ===  record.Id).length > 0);
         },
         action: (recordList: Array<AObject>): Observable<void> => {
           this.assetModalService.openBuyMoreModal(
@@ -505,7 +505,7 @@ export class AssetListComponent implements OnInit, OnDestroy {
         label: 'Change Configuration',
         theme: 'primary',
         validate(record: AssetLineItemExtended): boolean {
-          return record.canChangeConfiguration() && !_.includes(_.map(_.get(cart, 'LineItems'), 'ProductId'), _.get(record, 'ProductId'));
+          return record.canChangeConfiguration() && !(_.filter(_.get(cart, 'LineItems'), (item) => item.AssetLineItem.Id ===  record.Id).length > 0);
         },
         action: (recordList: Array<AObject>): Observable<void> => {
           this.assetModalService.openChangeConfigurationModal(
