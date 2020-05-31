55408c218cc00032b8b94c9b478a10549c51da7a
Dhrupit <dhdave@apttus.com>
2020-05-31 11:23:20 +0530

    compile issue
    compile issue

--- src/app/modules/products/services/product-details.resolver.ts
+++ src/app/modules/products/services/product-details.resolver.ts
@@ -14,6 +14,7 @@ import {
 import { Observable, zip, BehaviorSubject, Subscription } from 'rxjs';
 import { take, map, tap, filter, switchMap } from 'rxjs/operators';
 import { isNil, get, first } from 'lodash';
+import _ from 'lodash';
 
 @Injectable({
   providedIn: 'root'
