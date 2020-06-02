0c315844da57aaf60dce76474b96dcaf0d69a141
divinabopanna <dboppana@apttus.com>
2020-06-02 19:38:11 +0530

    Added check not to change the url for non configuration product.

--- src/app/modules/products/detail/product-detail.component.ts
+++ src/app/modules/products/detail/product-detail.component.ts
@@ -78,7 +78,7 @@ export class ProductDetailComponent implements OnInit {
   onAddToCart(cartItems: Array<CartItem>): void {
     this.configurationChanged = false;
     const primaryItem = _.find(cartItems, i => _.get(i, 'IsPrimaryLine') === true && _.isNil(_.get(i, 'Option')));
-    if (!_.isNil(primaryItem)) {
+    if (!_.isNil(primaryItem) && (_.get(primaryItem, 'Product.HasOptions') || _.get(primaryItem, 'Product.HasAttributes'))) {
       this.router.navigate(['/products', _.get(this, 'viewState$.value.product.Id'), _.get(primaryItem, 'Id')]);
     }
 
