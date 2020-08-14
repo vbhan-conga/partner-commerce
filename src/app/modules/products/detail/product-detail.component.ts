fca1c05107670ef1e0bfab4842d9a2e765f4ab79
divinabopanna <dboppana@apttus.com>
2020-08-14 17:16:02 +0530

    DC-5226

--- src/app/modules/products/detail/product-detail.component.ts
+++ src/app/modules/products/detail/product-detail.component.ts
@@ -1,103 +1,112 @@
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { CartService, CartItem, Storefront, StorefrontService, BundleProduct } from '@apttus/ecommerce';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { CartService, CartItem, BundleProduct } from '@apttus/ecommerce';
-import { BehaviorSubject } from 'rxjs';
-import { take } from 'rxjs/operators';
 import * as _ from 'lodash';
-import { ProductConfigurationSummaryComponent } from '@apttus/elements';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { ProductConfigurationSummaryComponent, ProductConfigurationService } from '@apttus/elements';
 import { ProductDetailsState, ProductDetailsResolver } from '../services/product-details.resolver';
+import { take } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-product-detail',
-  templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+    selector: 'app-product-detail',
+    templateUrl: './product-detail.component.html',
+    styleUrls: ['./product-detail.component.scss']
 })
-export class ProductDetailComponent implements OnInit {
+/**
+ * Product Details Component is the details of the product for standalone and bundle products with attributes and options.
+ */
+export class ProductDetailComponent implements OnInit, OnDestroy {
 
-  cartItemList: Array<CartItem>;
-  product: BundleProduct;
+    cartItemList: Array<CartItem>;
+    product: BundleProduct;
+    viewState$: BehaviorSubject<ProductDetailsState>;
 
-  viewState$: BehaviorSubject<ProductDetailsState>;
+    /**
+     * Flag to detect if there is change in product configuration.
+     */
+    configurationChanged: boolean = false;
 
-  /**
-   * Flag to detect if their is change in product configuration.
-   */
-  configurationChanged = false;
+    quantity: number = 1;
+    /**
+     * Flag used in update configuration method
+     */
+    saving: boolean = false;
+    /**
+     * Default term is set to 1.
+     */
+    term: number = 1;
 
-  quantity = 1;
+    /** @ignore */
+    productCode: string;
 
-  /**
-   * Flag used in update configuration method
-   */
-  saving = false;
 
-  /**
-   * Default term is set to 1.
-   */
-  term = 1;
+    @ViewChild(ProductConfigurationSummaryComponent, { static: false })
+    configSummaryModal: ProductConfigurationSummaryComponent;
+    subscription: Subscription;
 
-  /** @ignore */
-  productCode: string;
+    constructor(private cartService: CartService,
+                private resolver: ProductDetailsResolver,
+                private router: Router,
+                private activatedRoute: ActivatedRoute,
+                private productConfigurationService: ProductConfigurationService) { }
 
-  @ViewChild(ProductConfigurationSummaryComponent, { static: false })
-  configSummaryModal: ProductConfigurationSummaryComponent;
-
-  constructor(private cartService: CartService,
-    private resolver: ProductDetailsResolver,
-    private router: Router,
-    private activatedRoute: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.resolver
+    ngOnInit() {
+      this.resolver
       .resolve(this.activatedRoute.snapshot)
       .pipe(take(1))
       .subscribe(() => (this.viewState$ = this.resolver.state()));
-  }
-
-  /**
-   * onConfigurationChange method is invoked whenever there is change in product configuration and this method ets flag
-   * isConfigurationChanged to true.
-   */
-  onConfigurationChange(result: any) {
-    this.product = _.first(result);
-    this.cartItemList = result[1];
-    if (_.get(_.last(result), 'optionChanged') || _.get(_.last(result), 'attributeChanged')) this.configurationChanged = true;
-  }
-
-  /**
-   * Changes the quantity of the cart item passed to this method.
-   *
-   * @param cartItem Cart item reference to the cart line item object.
-   * @fires CartService.updateCartItems()
-   */
-
-  handleStartChange(cartItem: CartItem) {
-    this.cartService.updateCartItems([cartItem]);
-  }
-
-  onAddToCart(cartItems: Array<CartItem>): void {
-    this.configurationChanged = false;
-    const primaryItem = _.find(cartItems, i => _.get(i, 'IsPrimaryLine') === true && _.isNil(_.get(i, 'Option')));
-    if (!_.isNil(primaryItem) && (_.get(primaryItem, 'Product.HasOptions') || _.get(primaryItem, 'Product.HasAttributes'))) {
-      this.router.navigate(['/products', _.get(this, 'viewState$.value.product.Id'), _.get(primaryItem, 'Id')]);
+        this.subscription = this.productConfigurationService.configurationChange.subscribe(response => {
+            this.product = response.product;
+            this.cartItemList = response.itemList;
+            if (_.get(response.configurationFlags,'optionChanged') || _.get(response.configurationFlags,'attributeChanged')) this.configurationChanged = true;
+        });
     }
 
-    if (this.quantity <= 0) {
-      this.quantity = 1;
+    /**
+     * Changes the quantity of the cart item passed to this method.
+     *
+     * @param cartItem Cart item reference to the cart line item object.
+     * @fires CartService.updateCartItems()
+     */
+
+    handleStartChange(cartItem: CartItem) {
+        this.cartService.updateCartItems([cartItem]);
     }
-  }
 
-  /**
-   * Changes the quantity of the cart item passed to this method.
-   *
-   * @param cartItem Cart item reference to the cart line item object.
-   * @fires CartService.updateCartItems()
-   */
-  handleEndDateChange(cartItem: CartItem) {
-    this.cartService.updateCartItems([cartItem]);
-  }
+    onAddToCart(cartItems: Array<CartItem>): void {
+        this.configurationChanged = false;
+        const primaryItem = _.find(cartItems, i => _.get(i, 'IsPrimaryLine') === true && _.isNil(_.get(i, 'Option')));
+        if (!_.isNil(primaryItem) && (_.get(primaryItem, 'Product.HasOptions') || _.get(primaryItem, 'Product.HasAttributes'))) {
+            this.router.navigate(['/products', _.get(this, 'viewState$.value.product.Id'), _.get(primaryItem, 'Id')]);
+        }
+        if (this.quantity <= 0) {
+            this.quantity = 1;
+        }
+    }
 
-  showSummary() {
-    this.configSummaryModal.show();
-  }
+    changeProductQuantity(newQty: any){
+        if(this.cartItemList && this.cartItemList.length > 0)
+            _.forEach(this.cartItemList, c => {
+                if(c.LineType === 'Product/Service') c.Quantity = newQty;
+                this.productConfigurationService.changeProductQuantity(newQty);
+            });
+    }
+
+    /**
+     * Changes the quantity of the cart item passed to this method.
+     *
+     * @param cartItem Cart item reference to the cart line item object.
+     * @fires CartService.updateCartItems()
+     */
+    handleEndDateChange(cartItem: CartItem) {
+        this.cartService.updateCartItems([cartItem]);
+    }
+
+    showSummary() {
+      this.configSummaryModal.show();
+    }
+
+    ngOnDestroy(){
+        if(this.subscription) this.subscription.unsubscribe();
+    }
 }
