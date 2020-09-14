9f682962fdf6da7720e47a89886e6195273d17bd
divinabopanna <dboppana@apttus.com>
2020-09-14 16:54:08 +0530

    DC-5240

--- src/app/modules/products/detail/product-detail.component.ts
+++ src/app/modules/products/detail/product-detail.component.ts
@@ -26,6 +26,10 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
      * Flag to detect if there is change in product configuration.
      */
     configurationChanged: boolean = false;
+    /**
+     * Flag to detect if there is pending in product configuration.
+     */
+    configurationPending: boolean = false;
 
     quantity: number = 1;
     /**
@@ -67,10 +71,12 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
                 this.relatedTo = this.viewState$.value.relatedTo;
             });
         this.subscriptions.push(this.productConfigurationService.configurationChange.subscribe(response => {
+            if(response && _.has(response, 'configurationPending')) this.configurationPending = _.get(response,'configurationPending');
+            else if(_.get(response,'product')) {
             this.product = _.get(response,'product');
             this.cartItemList = _.get(response,'itemList');
             if (_.get(response, 'configurationFlags.optionChanged') || _.get(response, 'configurationFlags.attributeChanged')) this.configurationChanged = true;
-        }));
+        }}));
     }
 
     /**
