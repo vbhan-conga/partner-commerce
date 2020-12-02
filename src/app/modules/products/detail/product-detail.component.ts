94863f5d9361d57c78157851ca98ed8b9cc7b4be
divinabopanna <dboppana@apttus.com>
2020-12-02 17:46:11 +0530

    DC-6017

--- src/app/modules/products/detail/product-detail.component.ts
+++ src/app/modules/products/detail/product-detail.component.ts
@@ -70,7 +70,7 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
             });
         this.subscriptions.push(this.productConfigurationService.configurationChange.subscribe(response => {
             this.relatedTo = _.get(this.viewState$, 'value.relatedTo');
-            if (response && _.has(response, 'configurationPending')) this.configurationPending = _.get(response, 'configurationPending');
+            if (response && _.has(response, 'hasErrors')) this.configurationPending = _.get(response, 'hasErrors');
             else {
                 this.product = _.get(response, 'product');
                 this.cartItemList = _.get(response, 'itemList');
