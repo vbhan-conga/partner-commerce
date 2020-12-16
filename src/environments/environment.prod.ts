7bb31ecb04fc6ef388c9086e2c264fd1e1a56f7e
vbhan-conga <vbhan@apttus.com>
2020-12-16 17:53:31 +0530

    DC-6066

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -23,6 +23,8 @@ export const environment: Configuration = {
   useIndexedDB: true,
   expandDepth: 8,
   hashRouting: true,
+  skipPricing: true,
+  skipRules: false,
   packageNamespace: 'Apttus_WebStore',
   // Salesforce environment variables
   storefront: 'Partner Commerce',
