1aa81ff3c5cf6b478214d92189d6e1afaa908874
manoharcm-conga <63354549+manoharcm-conga@users.noreply.github.com>
2021-01-01 11:20:13 +0530

    Update environment.dc2.ts

--- src/environments/environment.dc2.ts
+++ src/environments/environment.dc2.ts
@@ -24,7 +24,7 @@ export const environment: Configuration = {
   useIndexedDB: true,
   expandDepth: 8,
   hashRouting: true,
-  skipPricing: false,
+  skipPricing: true,
   skipRules: false,
   packageNamespace: 'Apttus_WebStore',
   // *** TODO: Replace with Salesforce environment variables ***
