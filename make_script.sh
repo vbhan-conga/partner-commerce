cee05a0fd5cbb5ea577d106938c6d0fd49aa9003
manoharcm-conga <63354549+manoharcm-conga@users.noreply.github.com>
2021-02-06 08:03:36 +0530

    Build fix

--- make_script.sh
+++ make_script.sh
@@ -23,12 +23,12 @@ function npm_install {
     cd $packageJSON_Folder
     rm -rf node_modules
     LOG_INFO "Update NPM"
-    npm install -g npm
+    npm install -g npm --legacy-peer-deps
     LOG_INFO "Set Registry"
     npm config set registry https://registry.npmjs.com/
     LOG_INFO "NPM Install"
     npm set unsafe-perm true
-    npm install --unsafe-perm --no-package-lock
+    npm install --unsafe-perm --no-package-lock --legacy-peer-deps
     export NODE_OPTIONS=--max_old_space_size=4096
     npm install --unsafe-perm --max_old_space_size=4096 --legacy-peer-deps
 }
