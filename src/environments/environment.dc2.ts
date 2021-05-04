5a8dedae34801b84f241f7eae8f84ca0c87e7497
manoharcm-conga <63354549+manoharcm-conga@users.noreply.github.com>
2021-05-04 12:52:00 +0530

    skipPricing: true

--- src/environments/environment.dc2.ts
+++ src/environments/environment.dc2.ts
@@ -24,7 +24,7 @@ export const environment: Configuration = {
   useIndexedDB: true,
   expandDepth: 8,
   hashRouting: true,
-  skipPricing: false,
+  skipPricing: true,
   skipRules: false,
   apiVersion: '3',
   packageNamespace: 'Apttus_WebStore',
