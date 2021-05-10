727c8ea9a1a1620049ed9d51007d69e8249e8656
divinabopanna <dboppana@apttus.com>
2021-05-10 19:47:40 +0530

    DC-6634

--- src/app/modules/products/list/product-list.component.ts
+++ src/app/modules/products/list/product-list.component.ts
@@ -116,16 +116,10 @@ export class ProductListComponent implements OnInit, OnDestroy {
           this.category = new Category();
           this.category.Id = get(params, 'categoryId');
           categories = [get(params, 'categoryId')];
-          return this.categoryService.getCategoryBranchChildren(categories)
-            .pipe(mergeMap(result => {
-              if(result) categories = result.map(r => r.Id);
-              return this.productService.getProducts(categories, this.pageSize, this.page, sortBy, 'ASC', this.searchString, this.conditions);
-            }));
         } else if (!isEmpty(this.subCategories)) {
           categories = this.subCategories.map(category => category.Id);
-          return this.productService.getProducts(categories, this.pageSize, this.page, sortBy, 'ASC', this.searchString, this.conditions);
-        } else
-          return this.productService.getProducts(categories, this.pageSize, this.page, sortBy, 'ASC', this.searchString, this.conditions);
+        }
+        return this.productService.getProducts(categories, this.pageSize, this.page, sortBy, 'ASC', this.searchString, this.conditions);
       }),
     ).subscribe(r => {
       this.data$.next(r);
