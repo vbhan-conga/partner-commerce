8b437f4c0dc66450a10a0f7c5f8d665f4ae69220
jpatel-conga <jpatel@conga.com>
2021-06-01 17:49:18 +0530

    DC-6659 : [TP/CP] In product details page -> update qty from minicart and then select/deselect options and perform update configuration - qty is resetting back.

--- src/app/services/configure.guard.ts
+++ src/app/services/configure.guard.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanDeactivate } from '@angular/router';
-import { Observable } from 'rxjs';
-import { ProductService, Product, ConstraintRuleService } from '@apttus/ecommerce';
-import * as _ from 'lodash';
+import { Observable, of } from 'rxjs';
+import { map, filter, distinctUntilKeyChanged, flatMap, switchMap, take } from 'rxjs/operators';
+import { get, some } from 'lodash';
 import { ACondition, ConfigurationService } from '@apttus/core';
+import { ProductService, Product, ConstraintRuleService, CartService } from '@apttus/ecommerce';
 import { ProductDetailComponent } from '../modules/products/detail/product-detail.component';
-import { map, filter, distinctUntilKeyChanged, flatMap } from 'rxjs/operators';
+
 
 @Injectable()
 export class ConfigureGuard implements CanActivate, CanDeactivate<ProductDetailComponent>{
 
-    constructor(private router: Router, private productService: ProductService, private constraintRuleService: ConstraintRuleService, private config: ConfigurationService) { }
+    constructor(private router: Router, private productService: ProductService, private constraintRuleService: ConstraintRuleService, private config: ConfigurationService, private cartService: CartService) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
         return this.productService.where([new ACondition(Product, this.config.get('productIdentifier'), 'Equal', route.params.productCode)])
@@ -23,8 +24,8 @@ export class ConfigureGuard implements CanActivate, CanDeactivate<ProductDetailC
                         .pipe(
                             map(rules => {
                                 const activate =
-                                    ((_.get(product, 'Apttus_Config2__HasAttributes__c') && _.get(product, 'Apttus_Config2__AttributeGroups__r', []).totalSize > 0)
-                                        || (_.get(product, 'Apttus_Config2__HasOptions__c') && _.get(product, 'Apttus_Config2__OptionGroups__r', []).totalSize > 0))
+                                    ((get(product, 'Apttus_Config2__HasAttributes__c') && get(product, 'Apttus_Config2__AttributeGroups__r', []).totalSize > 0)
+                                        || (get(product, 'Apttus_Config2__HasOptions__c') && get(product, 'Apttus_Config2__OptionGroups__r', []).totalSize > 0))
                                     && rules.filter(rule => rule.ConstraintRuleActions.filter(action => action.ActionType === 'Replacement').length > 0).length === 0;
                                 if (!activate)
                                     this.router.navigate(['/products', product[this.config.get('productIdentifier')]]);
@@ -37,14 +38,19 @@ export class ConfigureGuard implements CanActivate, CanDeactivate<ProductDetailC
     }
 
     canDeactivate(component: ProductDetailComponent, route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-        if (component.configurationChanged) {
-            if (confirm('You have unsaved changes to the product configuration! Are you sure you want to proceed?'))
-                return true;
-            else
-                return false;
-        }
-        else
-            return true;
+        return this.cartService.getMyCart().pipe(
+            take(1),
+            switchMap(cart => { 
+                if (component.configurationChanged && some(cart.LineItems, { Id: get(route.params, 'cartItem')})) {
+                    if (confirm('You have unsaved changes to the product configuration! Are you sure you want to proceed?'))
+                        return of(true);
+                    else
+                        return of(false);
+                }
+                else
+                    return of(true);
+            })
+        );
     }
 
 }
