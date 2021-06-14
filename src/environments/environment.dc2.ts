0180571d165ef53e01fc3dd5395ac4c92923f52b
manoharcm-conga <63354549+manoharcm-conga@users.noreply.github.com>
2021-06-14 10:38:29 +0530

    CPQ Pricing mode

--- src/environments/environment.dc2.ts
+++ src/environments/environment.dc2.ts
@@ -27,7 +27,7 @@ export const environment: Configuration = {
   skipPricing: true,
   skipRules: false,
   apiVersion: '3',
-  pricingMode: 'turbo',
+  pricingMode: 'default',
   packageNamespace: 'Apttus_WebStore',
   // *** TODO: Replace with Salesforce environment variables ***
   storefront: 'P-Commerce',
