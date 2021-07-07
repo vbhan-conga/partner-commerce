3177995314f9afa82731b62ad8bebd45b1f88179
manoharcm-conga <63354549+manoharcm-conga@users.noreply.github.com>
2021-07-07 16:55:13 +0530

    TP build

--- src/environments/environment.dc2.ts
+++ src/environments/environment.dc2.ts
@@ -27,7 +27,7 @@ export const environment: Configuration = {
   skipPricing: true,
   skipRules: false,
   apiVersion: '3',
-  pricingMode: 'default',
+  pricingMode: 'turbo',
   packageNamespace: 'Apttus_WebStore',
   // *** TODO: Replace with Salesforce environment variables ***
   storefront: 'P-Commerce',
