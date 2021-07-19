62adeab27b84ff682c02b7edc7558033cf313489
vbhan-conga <63386516+vbhan-conga@users.noreply.github.com>
2021-07-19 16:19:28 +0530

    Updated pricingMode to default

--- src/environments/environment.dc4.ts
+++ src/environments/environment.dc4.ts
@@ -14,7 +14,7 @@ export const environment: Configuration = {
   maxBufferSize: 10,
   disableBuffer: false,
   subqueryLimit: 10,
-  pricingMode: 'turbo',
+  pricingMode: 'default',
   disableCache: false,
   encryptResponse: false,
   cartRetryLimit: 3,
